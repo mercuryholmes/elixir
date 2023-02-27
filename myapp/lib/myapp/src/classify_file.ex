a23dd2cdb3139eaf66fe8d43972b7551a06d3e37
mercuryholmes <mercury.milky.holmes@gmail.com>
2023-02-28 06:04:03 +0900

    リファクタ

--- myapp/lib/myapp/src/classify_file.ex
+++ myapp/lib/myapp/src/classify_file.ex
@@ -5,21 +5,19 @@ defmodule ClassifyFile do
   @pattern %{"\.md$" => "md"} # {Regex => Directory}
 
   def exec() do
-    File.ls!(@path) |> Enum.map(&classify(&1))
+    File.ls!(@path)
+    |> Enum.each(&classify(&1))
   end
 
   defp classify(file) do
-      Enum.map(@pattern, fn ({pattern, directory_name}) ->
+      Enum.map(@pattern, fn ({pattern, directory}) ->
+          dest = Path.join(@path, directory)
+          File.mkdir_p!(dest)
           if file =~ Regex.compile!(pattern) do
-              mkdir_if_not_exists("#{@path}#{directory_name}")
-              File.rename("#{@path}#{file}", "#{@path}#{directory_name}/#{file}")
+            File.rename(file, Path.join(dest, file))
+            IO.puts "「#{file}」を「#{directory}」フォルダへ移動しました。"
           end
-        end
-      )
-  end
-
-  defp mkdir_if_not_exists(directory) do
-      unless File.exists?(directory) do File.mkdir!(directory) end
+      end)
   end
 
 end
